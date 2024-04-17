384c83ffd193611bb6bf6d3ef6b56154de4c518e
Sarah Sameh <sarahsameh627@gmail.com>
2024-04-17 11:49:52 +0200

    created new directory for images

---
+++ Test Files OOP/OOP/src/main/java/preparing.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author workstation
+ */
+public class preparing {
+    public void printNumbers(int number){
+        System.out.println("this is the number you've entered " + number);
+    }
+    
+}
---
+++ Test Files OOP/OOP/src/main/java/testingclass.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author workstation
+ */
+
+
+
+public class testingclass {
+    
+
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String s1 = new String("Welcome");
+        String s2 = new String("Welcome");;
+        System.out.println(s1.equals(s2));
+            
+        }
+    
+    static {
+        System.out.println("We are inside the static block");
+    }
+        
+        
+        
+    }
+    
+
