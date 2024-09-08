76b15b5955c40d52d53cc7a121f18dcfd0446d8b
Sarah Sameh <sarahsameh627@gmail.com>
2024-09-08 20:18:35 +0300

    edited the return statement in calculateDistance method

--- Methods/src/Java/repo/Main.java
+++ Methods/src/Java/repo/Main.java
@@ -25,7 +25,7 @@ public class Main {
     public static double calculateDistance(int x1, int x2 , int y1, int y2){
         int deltaX = x2 - x1;
         int deltaY = y2 - y1;
-        double distance = Math.sqrt(Math.pow(deltaX,2) + Math.pow(deltaY,2) );
-        return distance;
+        return  Math.sqrt(Math.pow(deltaX,2) + Math.pow(deltaY,2) );
+
     }
 }
