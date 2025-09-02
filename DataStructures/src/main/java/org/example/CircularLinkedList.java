3bad7ff719db323a4a3f0e3b5eeb8036728773b8
Sarah <sarahsameh627@gmail.com>
2025-09-02 04:37:59 +0300

    added implementation for circular linked list made for round robin algorithm of CPU scheduling

--- DataStructures/src/main/java/org/example/CircularLinkedList.java
+++ DataStructures/src/main/java/org/example/CircularLinkedList.java
@@ -1,6 +1,55 @@
 package org.example;
 
-public class CircularLinkedList {
+public class CircularLinkedList <T> {
+    static class Node<T>{
+        T element;
+        Node<T> next;
 
-    
+        public Node(T element, Node<T> next){
+            this.element = element;
+            this.next = next;
+        }
+    }
+    private Node<T> tail = null;
+    private int size = 0;
+
+    public boolean isEmpty(){
+        return (size == 0);
+    }
+
+    public void insert(T element){
+        if(isEmpty()){
+            tail = new Node<>(element,null);
+            tail.next=tail;
+        }else{
+            tail.next = new Node<>(element,tail);
+        }
+        size++;
+    }
+
+    public void remove(T element){
+        if(isEmpty()){
+            return;
+        }
+        Node<T> head = tail.next;
+        if(head == tail) tail = null;
+        else{
+            tail.next = head.next;
+        }
+        size--;
+    }
+
+
+
+    public void rotate(){
+        if(tail != null){
+            tail=tail.next;
+        }
+    }
+
+
+
+
+
+
 }
