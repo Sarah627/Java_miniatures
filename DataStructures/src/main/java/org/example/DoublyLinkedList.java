053c56670ad184e4eb811dd20c126ec23ee513b3
Sarah <sarahsameh627@gmail.com>
2025-09-02 04:44:15 +0300

    added Node classes for both doubly linked list classes

--- DataStructures/src/main/java/org/example/DoublyLinkedList.java
+++ DataStructures/src/main/java/org/example/DoublyLinkedList.java
@@ -2,4 +2,15 @@ package org.example;
 
 public class DoublyLinkedList {
 
+    static class Node{
+        int element;
+        Node prev;
+        Node next;
+
+        public Node(int element, Node next, Node prev){
+            this.element = element;
+            this.next = next;
+            this.prev = prev;
+        }
+    }
 }
--- DataStructures/src/main/java/org/example/GenericDoubleLinkedList.java
+++ DataStructures/src/main/java/org/example/GenericDoubleLinkedList.java
@@ -1,4 +1,20 @@
 package org.example;
 
 public class GenericDoubleLinkedList <T>{
+    static class Node<T>{
+        T element;
+        Node<T> next;
+        Node<T> prev;
+
+        public Node(T element, Node<T> next, Node<T> prev){
+            this.element = element;
+            this.next = next;
+            this.prev = prev;
+        }
+    }
+
+
+    
+
+
 }
