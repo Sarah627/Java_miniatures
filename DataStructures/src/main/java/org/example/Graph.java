1e1c083aadef3ce625d3963abe7a78fd3a6746a2
Sarah <sarahsameh627@gmail.com>
2025-10-22 05:51:29 +0300

    added graph class to represent a graph using adjecency list graph representation with an array list

--- DataStructures/src/main/java/org/example/Graph.java
+++ DataStructures/src/main/java/org/example/Graph.java
@@ -1,4 +1,58 @@
 package org.example;
 
+import java.util.ArrayList;
+
 public class Graph {
+    private ArrayList<ArrayList<Integer>> graph;
+
+    int v;
+
+
+    Graph(int nodes){
+        v =nodes;
+        graph = new ArrayList<>();
+        for(int i =0;i<v;i++){
+            graph.add(new ArrayList<>());
+        }
+    }
+
+    void addEdge(int v, int u){
+        graph.get(u).add(v);
+        graph.get(v).add(u);
+    }
+
+    void printGraph(){
+        for(int i =0;i<v;i++){
+            System.out.print("Node "+ i+ " -> ");
+            System.out.println(graph.get(i));
+        }
+    }
+
+    void BFS(int v){
+        QueueListBased toBeVisited = new QueueListBased();
+        ArrayList<Integer> visited = new ArrayList<>();
+        if (v >= graph.size()){
+            throw new IndexOutOfBoundsException("this node doesn't exist");
+        }else{
+            toBeVisited.enqueue(v);
+            visited.add(v);
+            while(!toBeVisited.isEmpty()){
+                int list = toBeVisited.dequeue();
+                System.out.println(list);
+                    for (int i = 0; i < graph.get(list).size(); i++) {
+                        int currentElement= graph.get(list).get(i);
+                        if (visited.contains(currentElement)) continue;
+                        visited.add(currentElement);
+                        toBeVisited.enqueue(currentElement);
+                    }
+
+
+
+
+            }
+
+
+        }
+    }
+
 }
