ee5647aeec946d4ef69eaa56ebfd9beb97598a31
Sarah <sarahsameh627@gmail.com>
2025-10-22 06:04:22 +0300

    implemented Queue class based on linked list

--- DataStructures/src/main/java/org/example/QueueListBased.java
+++ DataStructures/src/main/java/org/example/QueueListBased.java
@@ -1,4 +1,27 @@
 package org.example;
 
 public class QueueListBased {
+    private LinkedList queue;
+    QueueListBased(){
+        queue = new LinkedList();
+    }
+
+    void enqueue(int value){
+        queue.append(value);
+    }
+
+    int dequeue(){
+        int element = queue.first();
+        queue.removeAt(0);
+        return element;
+    }
+
+    int size(){
+        return queue.size();
+    }
+
+    boolean isEmpty(){
+        return queue.isEmpty();
+    }
+
 }
