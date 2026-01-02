ac64eeaad68d3333b8701d7ce1693f3a00287714
Sarah <sarahsameh627@gmail.com>
2026-01-02 04:18:13 +0200

    built the Graph Matrix Based class for building Mazes

---
+++ DataStructures/src/main/java/org/example/GraphMatrixBased.java
@@ -0,0 +1,20 @@
+package org.example;
+
+import java.util.ArrayList;
+
+public class Cell {
+    int r, c;
+    Cell[] prev;
+    ArrayList<Cell> neighbors = new ArrayList<>();
+
+
+    public Cell(int rows,int columns){
+
+        for (int i = 0; i < rows; i++) {
+            for(int j=0;j<columns;j++){
+                
+            }
+        }
+
+    }
+}
